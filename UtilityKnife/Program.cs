f3f8b28473b638a5bb95e891db06a69384b99204
Christopher Allan <2202516@uad.ac.uk>
2024-10-11 17:47:28 +0100

    Added skeleton

---
+++ UtilityKnife/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace UtilityKnife
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            // skeleton program
+        }
+    }
+}
