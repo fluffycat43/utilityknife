7da7bfe81ad3877888f24b123605c95b9f3c316e
Christopher Allan <2202516@uad.ac.uk>
2024-10-11 19:24:33 +0100

    made a test

--- UtilityKnife/Program.cs
+++ UtilityKnife/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using UtilityKnife.Converters;
+
 namespace UtilityKnife
 {
     public static class Program
@@ -6,6 +8,9 @@ namespace UtilityKnife
         static void Main(string[] args)
         {
             // skeleton program
+            var converter = new FahrenheitToCelsius();
+            var celsius = converter.FahrenheitToCelsiusConverter(212.0);
+            Console.WriteLine($"Fahrenheit temp of 212 is {celsius}.");
         }
     }
 }
