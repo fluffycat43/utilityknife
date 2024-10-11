787a318cd9efd380d8dc0f25b1a179265d596b5f
James Murphy <2201705@uad.ac.uk>
2024-10-11 19:15:54 +0100

    updated converter

--- UtilityKnife/Converters/FahrenheitToCelsius.cs
+++ UtilityKnife/Converters/FahrenheitToCelsius.cs
@@ -10,8 +10,10 @@ namespace UtilityKnife.Converters
     {
            public double FahrenheitToCelsiusConverter(double FahrenheitTemp)
             {
-                double _fehrenheitTemp = 0.0;
                 double _celsius = 0.0;
+                var step1 = Calculator.Calculator.Subtract(FahrenheitTemp, 32);
+                var step2 = Calculator.Calculator.Multiply(step1, 5.0);
+                _celsius = Calculator.Calculator.Division(step2, 9.0);
                 return _celsius;
             }
         }
